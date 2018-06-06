b262abc1028bd7512462fe59fb2f8363a3a54ec7
Deepak <hello.dk@outlook.com>
2018-06-06 11:25:32 +0530

    Mass commit

--- shell_utilities/golang_ubuntu.sh
+++ shell_utilities/golang_ubuntu.sh
@@ -11,4 +11,4 @@ sudo wget https://dl.google.com/go/go1.10.2.linux-amd64.tar.gz
 sudo tar -C /usr/local/ -xzf go1.10.2.linux-amd64.tar.gz
 # vim /etc/profile
 # export PATH=$PATH:/usr/local/go/bin
-echo “export PATH=$PATH:/usr/local/go/bin” >> /etc/profile
+echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
