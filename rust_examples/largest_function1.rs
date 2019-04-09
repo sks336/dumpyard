51ff4249fec574a9b2b6fe03f096bb446e7ae9fc
hellodk <hello.dk@outlook.com>
2019-04-09 14:36:37 +0530

    Added few more rust files

---
+++ rust_examples/largest_function1.rs
@@ -0,0 +1,25 @@
+// will not compile - need to add traits in this
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T { // Added the traits here
+// fn largest<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+}
--- rust_examples/variables/src/char.rs
+++ rust_examples/variables/src/char.rs
@@ -1,4 +1,9 @@
 fn main() {
    let c = 'z';
    println!("c is : {}", c);
+   let s = String::from("hello");
+   println!("String is : {}", s);
+   let mut s1 = String::from("hello");
+   s1.push_str(", world!"); // push_str() appends a literal to a String
+   println!("{}", s1); // this will print `hello, world!`
 }
