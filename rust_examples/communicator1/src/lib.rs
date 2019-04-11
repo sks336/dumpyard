2bd8585fa0e356200c961d5e7992d12eb6765e05
hellodk <hello.dk@outlook.com>
2019-04-11 11:21:05 +0530

    Added files

---
+++ rust_examples/communicator1/src/lib.rs
@@ -0,0 +1,54 @@
+pub mod client;
+mod network;
+
+
+
+//mod server;
+
+/* mod client;
+mod network; */
+
+/* mod client;
+mod network {
+    fn connect() {
+    }
+    mod server {
+        fn connect() {
+        }
+    }
+} */
+
+
+// client, network, network:server
+
+/* mod client {
+    fn connect() {
+    }
+}
+mod network {
+    fn connect() {
+    }
+    mod server {
+        fn connect() {
+        }
+    }
+} */
+
+/*mod network {
+    fn connect() {
+    }
+    mod client {
+        fn connect() {
+        }
+    }
+} */
+
+
+/*mod network {
+    fn connect() {
+    }
+}
+mod client {
+    fn connect() {
+    }
+}*/
