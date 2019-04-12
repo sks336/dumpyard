e60727fccb1212774d49406a3b626b25c1e1739a
hellodk <hello.dk@outlook.com>
2019-04-12 14:36:55 +0530

    Added files

--- rust_examples/mutex.rs
+++ rust_examples/mutex.rs
@@ -1,12 +1,13 @@
-use std::sync::{Mutex, Arc};
+use std::rc::Rc; // Reference Counting
+use std::sync::Mutex;
 use std::thread;
 
 fn main() {
-    let counter = Arc::new(Mutex::new(0));
+    let counter = Rc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10 {
-        let counter = Arc::clone(&counter);
+        let counter = Rc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
 
